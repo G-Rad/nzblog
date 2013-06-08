115c99508c8cff507b178dc95034f03726dcb60a
Gleb Teterin <gleb.teterin@doneright.ru>
2013-06-09 10:12:27 +1200

    UnitTests : add project

---
+++ src/UnitTests/FlickrTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using FlickrNet;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class FlickrTests
+	{
+		[Test]
+		[Explicit]
+		public void ApiSpike_GetInfo()
+		{
+			var client = new Flickr("dummyapikey", "dummysharedsecret");
+			client.InstanceCacheDisabled = true;
+
+			var photos = client.PhotosSearch(new PhotoSearchOptions("dummyuserid", "blog"));
+
+			var result = new List<Tuple<string,string>>();
+
+			foreach (var photo in photos)
+			{
+				var info = client.PhotosGetInfo(photo.PhotoId, photo.Secret);
+				result.Add(new Tuple<string, string>(info.Title, info.Description));
+			}
+
+			Assert.That(result, Is.Not.Empty);
+		}
+	}
+}
