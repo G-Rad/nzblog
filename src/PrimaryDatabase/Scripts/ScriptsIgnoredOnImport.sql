748e1280669009a65d9d0244bbbe18fa737fe930
Gleb Teterin <gleb.teterin@doneright.ru>
2013-06-23 10:23:27 +1200

    feat(web): add elmah logging

---
+++ src/PrimaryDatabase/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,121 @@
+﻿
+/* ------------------------------------------------------------------------ 
+        STORED PROCEDURES                                                      
+   ------------------------------------------------------------------------ */
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
+
+SET ANSI_NULLS ON
+GO
+
+/*
+  
+   ELMAH - Error Logging Modules and Handlers for ASP.NET
+   Copyright (c) 2004-9 Atif Aziz. All rights reserved.
+  
+    Author(s):
+  
+        Atif Aziz, http://www.raboof.com
+        Phil Haacked, http://haacked.com
+  
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+  
+      http://www.apache.org/licenses/LICENSE-2.0
+  
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+  
+*/
+
+-- ELMAH DDL script for Microsoft SQL Server 2000 or later.
+
+-- $Id: SQLServer.sql 677 2009-09-29 18:02:39Z azizatif $
+
+DECLARE @DBCompatibilityLevel INT
+DECLARE @DBCompatibilityLevelMajor INT
+DECLARE @DBCompatibilityLevelMinor INT
+
+SELECT 
+    @DBCompatibilityLevel = cmptlevel 
+FROM 
+    master.dbo.sysdatabases 
+WHERE 
+    name = DB_NAME()
+
+IF @DBCompatibilityLevel <> 80
+BEGIN
+
+    SELECT @DBCompatibilityLevelMajor = @DBCompatibilityLevel / 10, 
+           @DBCompatibilityLevelMinor = @DBCompatibilityLevel % 10
+           
+    PRINT N'
+    ===========================================================================
+    WARNING! 
+    ---------------------------------------------------------------------------
+    
+    This script is designed for Microsoft SQL Server 2000 (8.0) but your 
+    database is set up for compatibility with version ' 
+    + CAST(@DBCompatibilityLevelMajor AS NVARCHAR(80)) 
+    + N'.' 
+    + CAST(@DBCompatibilityLevelMinor AS NVARCHAR(80)) 
+    + N'. Although 
+    the script should work with later versions of Microsoft SQL Server, 
+    you can ensure compatibility by executing the following statement:
+    
+    ALTER DATABASE [' 
+    + DB_NAME() 
+    + N'] 
+    SET COMPATIBILITY_LEVEL = 80
+
+    If you are hosting ELMAH in the same database as your application 
+    database and do not wish to change the compatibility option then you 
+    should create a separate database to host ELMAH where you can set the 
+    compatibility level more freely.
+    
+    If you continue with the current setup, please report any compatibility 
+    issues you encounter over at:
+    
+    http://code.google.com/p/elmah/issues/list
+
+    ===========================================================================
+'
+END
+
+
+GO
