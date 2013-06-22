7477126183088501efd8d9c7c17849b1f37cd01f
Gleb Teterin <gleb.teterin@doneright.ru>
2013-06-22 13:00:21 +1200

    feat(monitor): add a page for setting an analytics exclusion cookie

---
+++ src/Blog/Areas/Monitor/Controllers/ActionController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Areas.Monitor.Controllers
+{
+	public class ActionController : Controller
+	{
+		 public ActionResult Exclude()
+		 {
+			 return View();
+		 }
+	}
+}
