3fb172d28883e3937ce38240b2d8740abd8e76f9
Gleb Teterin <gleb.teterin@doneright.ru>
2013-06-22 13:05:18 +1200

    refactor(monitor): move actions to the Action controller

--- src/Blog/Areas/Monitor/Controllers/ActionController.cs
+++ src/Blog/Areas/Monitor/Controllers/ActionController.cs
@@ -1,12 +1,40 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+using NLog;
 
 namespace Web.Areas.Monitor.Controllers
 {
 	public class ActionController : Controller
 	{
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
 		 public ActionResult Exclude()
 		 {
 			 return View();
+		 }
+
+		 public ActionResult Exception()
+		 {
+			 throw new NullReferenceException("This a test exception");
+		 }
+
+		 public ActionResult Log()
+		 {
+			 Logger.Debug("This is a test debug log record");
+			 Logger.Warn("This is a test warn log record");
+			 Logger.Error("This is a test error log record");
+
+			 try
+			 {
+				 var s = ((ActionResult)null).ToString();
+			 }
+			 catch (Exception ex)
+			 {
+				 Logger.ErrorException("This is a test error log record", ex);
+			 }
+
+			 return new HttpStatusCodeResult(HttpStatusCode.OK);
 		 }
 	}
 }
--- src/Blog/Areas/Monitor/Controllers/InfoController.cs
+++ src/Blog/Areas/Monitor/Controllers/InfoController.cs
@@ -3,14 +3,11 @@ using System.Linq;
 using System.Net;
 using System.Web.Mvc;
 using Core.Services;
-using NLog;
 
 namespace Web.Areas.Monitor.Controllers
 {
 	public class InfoController : Controller
 	{
-		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-
 		private readonly IWebsiteInfoProvider _websiteInfoProvider;
 		private readonly IFlickrService _flickrService;
 
@@ -38,29 +35,6 @@ namespace Web.Areas.Monitor.Controllers
 								};
 
 			return Json(jsonResult, JsonRequestBehavior.AllowGet);
-		}
-
-		public ActionResult Exception()
-		{
-			throw new NullReferenceException("This a test exception");
-		}
-
-		public ActionResult Log()
-		{
-			Logger.Debug("This is a test debug log record");
-			Logger.Warn("This is a test warn log record");
-			Logger.Error("This is a test error log record");
-
-			try
-			{
-				var s = ((ActionResult) null).ToString();
-			}
-			catch (Exception ex)
-			{
-				Logger.ErrorException("This is a test error log record", ex);
-			}
-
-			return new HttpStatusCodeResult(HttpStatusCode.OK);
 		}
 	}
 }
